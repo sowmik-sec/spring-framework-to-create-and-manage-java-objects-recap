c9200bc56459934205bd0a9a82ad4a46fc8f48fc
sowmik-sec <sowmik.sec@gmail.com>
2023-11-11 11:40:07 +0600

    exploring primary and qualifier annotations for spring components

--- src/main/java/com/sowmik/learnspringframeworkrecap/game/GameRunner.java
+++ src/main/java/com/sowmik/learnspringframeworkrecap/game/GameRunner.java
@@ -1,5 +1,6 @@
 package com.sowmik.learnspringframeworkrecap.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,7 +8,7 @@ public class GameRunner {
     //private MarioGame game;
     private GamingConsole game;
 
-    public GameRunner(GamingConsole game) {
+    public GameRunner(@Qualifier("SuperContraGameQualifier") GamingConsole game) {
         this.game = game;
     }
     public void run() {
